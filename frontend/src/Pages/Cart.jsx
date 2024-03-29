d25fa9131b36c451015daae4ca1e0b9291711d68
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-03-30 00:22:49 +0530

    feat: Add CartItems component and its styles; Implement subtotal and total calculation in CartItems
    - Added CartItems component with its corresponding CSS file
    - Implemented subtotal and total calculation in CartItems using context
      API
    This commit enhances the cart functionality by introducing the CartItems
    component and ensuring proper calculation of subtotal and total amounts.

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import CartItems from "./../Components/CartItems/CartItems";
+
 function Cart() {
-    return <div>Cart</div>;
+    return (
+        <div>
+            <CartItems />
+        </div>
+    );
 }
 
 export default Cart;
