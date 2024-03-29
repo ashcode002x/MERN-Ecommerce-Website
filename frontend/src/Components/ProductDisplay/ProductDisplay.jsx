7dd7c584a09ef28a4cc73cba71cd0f12b5be1e13
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-03-29 23:22:22 +0530

    feat: Implement addToCart functionality in ProductDisplay component
    - Added onClick event to addToCart button in ProductDisplay component to
      add items to the cart
    - Implementd addToCart and removeFromCart functions in ShopContext to
      manage cart items
    
    This commit enhances the shopping experience by allowing users to add
    items to their  cart directly from product display page.

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
     const { product } = props;
+
+    const { addToCart } = useContext(ShopContext);
 
     return (
         <div className='productdisplay'>
@@ -62,7 +65,13 @@ const ProductDisplay = (props) => {
                     </div>
                 </div>
 
-                <button>ADD TO CART</button>
+                <button
+                    onClick={() => {
+                        addToCart(product.id);
+                    }}
+                >
+                    ADD TO CART
+                </button>
 
                 <p className='productdisplay-right-category'>
                     <span>Category :</span> Women, T-shirt, Crop Top
