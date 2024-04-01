3cef61f95178b3ef26521ad8ecb3c405f4df9187
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-01 17:22:10 +0530

    fix(admin): Adjust logo styling and replace old logo with new one

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,21 @@
 
 import "./Navbar.css";
 
-import nav_logo from "../../assets/new-logo.png";
+import nav_logo from "../../assets/logo.png";
 import nav_profile from "../../assets/nav-profile.svg";
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <img src={nav_logo} alt='' className='nav-logo' />
+            <div className='logo-container'>
+                <div className='logo-left'>
+                    <img src={nav_logo} alt='' />
+                </div>
+                <div className='logo-right'>
+                    <p className='main'>QuickCart</p>
+                    <p className='secondary'>Admin Panel</p>
+                </div>
+            </div>
             <img src={nav_profile} alt='' className='nav-profile' />
         </div>
     );
