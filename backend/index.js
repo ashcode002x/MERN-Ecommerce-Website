20e8ddb006741475a0b3e55cb8fc6fa9ac5eecd2
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-01 08:48:44 +0530

    feat: Add image upload functionality
    - Added multer disk storage for handlin image up;pads.
    - Implemented endpoint for uploading images.
    - Created endpoint for serving uploaded images.
    - Generated unique filename for uploaded images.

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,29 @@ app.get("/", (req, res) => {
     res.send("Express App is Running");
 });
 
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: "./upload/images",
+    filename: (req, file, cb) => {
+        return cb(
+            null,
+            `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+        );
+    },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for Images
+app.use("/images", express.static("upload/images"));
+
+app.post("/upload", upload.single("product"), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    });
+});
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("server running on port " + port);
