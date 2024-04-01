be905bc6ad85572e05ad0d05536de7ab137914e0
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-01 22:24:18 +0530

    feat: Add AddProduct and ListProduct components to Admin Page
    - Added AddProduct and ListProductt components to the Admin page for
      managing prodcts.
    - Created corresponding CSS files for styling.
    - Updated Admin CSS to adjust layout for smaller screens.

---
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,11 @@
+import "./AddProduct.css";
+
+const AddProduct = () => {
+    return (
+        <div className='add-product'>
+            <h1>saurav</h1>
+        </div>
+    );
+};
+
+export default AddProduct;
