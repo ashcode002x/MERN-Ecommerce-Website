c633d241367a05ac1b5ad560a9eb1b1ee974c9a9
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-01 23:00:57 +0530

    feat(admin): Implement styling and functionality for AddProduct component
    - Updated AddProduct.css to style the AddProduct component.
    - Modified AddProduct.jsx to implement form fields for product details
      and image upload functionality.
    - Implemented state management for product details and image in
      AddProduct.jsx.
    - Implemented changeHandler and imageHandler functions to handle form
      input changes and image upload.
    - Added a button to trigger the Add_Product function for adding a new
      product.

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,9 +1,102 @@
+import { useState } from "react";
+
 import "./AddProduct.css";
 
+import upload_area from "../../assets/upload_area.svg";
+
 const AddProduct = () => {
+    const [image, setImage] = useState(false);
+    const [productDetails, setProductDetails] = useState({
+        name: "",
+        image: "",
+        category: "women",
+        new_price: "",
+        old_price: "",
+    });
+
+    const imageHandler = (e) => {
+        setImage(e.target.files[0]);
+    };
+
+    const changeHandler = (e) => {
+        setProductDetails({
+            ...productDetails,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const Add_Product = async () => {
+        console.log(productDetails);
+    };
+
     return (
         <div className='add-product'>
-            <h1>saurav</h1>
+            <div className='add-product-itemField'>
+                <p>Product title</p>
+                <input
+                    value={productDetails.name}
+                    onChange={changeHandler}
+                    type='text'
+                    name='name'
+                    placeholder='Type here'
+                />
+            </div>
+            <div className='add-product-price'>
+                <div className='add-product-itemField'>
+                    <p>Price</p>
+                    <input
+                        value={productDetails.old_price}
+                        onChange={changeHandler}
+                        type='text'
+                        name='old_price'
+                        placeholder='Type here'
+                    />
+                </div>
+                <div className='add-product-itemField'>
+                    <p>Offer Price</p>
+                    <input
+                        value={productDetails.new_price}
+                        onChange={changeHandler}
+                        type='text'
+                        name='new_price'
+                        placeholder='Type here'
+                    />
+                </div>
+            </div>
+
+            <div className='add-product-itemField'>
+                <p>Product Category</p>
+                <select
+                    value={productDetails.category}
+                    onChange={changeHandler}
+                    name='category'
+                    className='add-product-selector'
+                >
+                    <option value='women'>Women</option>
+                    <option value='men'>Men</option>
+                    <option value='kid'>Kids</option>
+                </select>
+            </div>
+
+            <div className='add-product-itemField'>
+                <label htmlFor='file-input'>
+                    <img
+                        src={image ? URL.createObjectURL(image) : upload_area}
+                        className='add-product-thumbnail-img'
+                        alt=''
+                    />
+                </label>
+                <input
+                    onChange={imageHandler}
+                    type='file'
+                    name='image'
+                    id='file-input'
+                    hidden
+                />
+            </div>
+            <button onClick={Add_Product} className='add-product-button'>
+                ADD
+            </button>
         </div>
     );
 };
