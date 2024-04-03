d1f34876099335551a8b154f8f739b3d5c070862
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-03 16:08:03 +0530

    feat: Add cart management endpoints and user authentication middlerware
    - Added middleware to fetch user data from JWT token
    - Created endpoints to add, remove, and get cart data
    - Modified responses to send JSON instead of plain text
    - Updated ShopContext to manage cart data based on user authentication

--- backend/index.js
+++ backend/index.js
@@ -218,6 +218,60 @@ app.get("/popularinwomen", async (req, res) => {
     res.send(popular_in_women);
 });
 
+// Creating middleware to fetch user
+const fetchUser = async (req, res, next) => {
+    const token = req.header("auth-token");
+    if (!token) {
+        res.status(401).send({
+            errors: "Please authenticate using valid token",
+        });
+    } else {
+        try {
+            const data = jwt.verify(token, "secret_ecom");
+            req.user = data.user;
+            next();
+        } catch (error) {
+            res.status(401).send({
+                errors: "Please authenticate using a valid token",
+            });
+        }
+    }
+};
+
+// Creating endpoint for adding products to cart data
+app.post("/addtocart", fetchUser, async (req, res) => {
+    console.log("Added ", req.body.itemId);
+    let userData = await Users.findOne({ _id: req.user.id });
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate(
+        { _id: req.user.id },
+        { cartData: userData.cartData }
+    );
+
+    res.send("Added");
+});
+
+// Creating endpoint to remove products from cart data
+app.post("/removefromcart", fetchUser, async (req, res) => {
+    console.log("Removed ", req.body.itemId);
+    let userData = await Users.findOne({ _id: req.user.id });
+    if (userData.cartData[req.body.itemId] > 0)
+        userData.cartData[req.body.itemId] -= 1;
+    await Users.findOneAndUpdate(
+        { _id: req.user.id },
+        { cartData: userData.cartData }
+    );
+
+    res.send("Removed");
+});
+
+// Creating endpoint to get cart data
+app.post("/getcart", fetchUser, async (req, res) => {
+    console.log("GetCart");
+    let userData = await Users.findOne({ _id: req.user.id });
+    res.json(userData.cartData);
+});
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("server running on port " + port);
