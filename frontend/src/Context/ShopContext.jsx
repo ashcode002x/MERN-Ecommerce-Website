c841691c2d08e3ddb644f5ba5cdba9b693df4d35
SauravSinghKarmwar02 <100ravsinghkarmwar@gmail.com>
2024-04-03 05:49:46 +0530

    feat(frontend): Add functionality to fetch all products from server
    This commit adds a uuseEffect hook in ShopContextProvider to fetch all
    products from the server upon component mount. It uses the fetch API to
    make a GET request to 'http://localhost:4000/allporducts' and updates
    the state with the received data.

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,12 +1,11 @@
-import React, { createContext, useState } from "react";
-import all_product from "./../Components/Assets/all_product";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
 
-    for (let index = 0; index < all_product.length + 1; index++) {
+    for (let index = 0; index < 300 + 1; index++) {
         cart[index] = 0;
     }
 
@@ -15,6 +14,14 @@ const getDefaultCart = () => {
 
 const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    const [all_product, setAll_product] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/allproducts")
+            .then((response) => response.json())
+            .then((data) => setAll_product(data));
+    }, []);
 
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
